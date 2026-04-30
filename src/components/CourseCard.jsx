034c6676c9bbae51693037ab6affbe90833304fb
AfazurRahman <afazurr8@gmail.com>
2026-04-30 14:02:57 +0600

    upload card images again after reducing image resulation

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import React from 'react';
 
 const CourseCard = ({course}) => {
-    const {image, title,instructor, rating} =course;
+    const {image, title,instructor, rating} =course; 
     return (
         <Card>
             <div>
